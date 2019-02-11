1c735d3f9162c50b719f6d23feea6717844bb4b3
Ben Li <admin@onesway.xyz>
2019-02-11 12:04:04 +0800

    feat(data): add base data temp file #1

---
+++ client/data/exits.temp.js
@@ -0,0 +1,17 @@
+export default {
+  xxxx: {
+    bus: [
+      {
+        common: ['xxxxx','xxxx'],
+      },
+    ],
+    exit: [
+      {
+        name: 'xxxx',
+        content: ['xxxxxxx'],
+      },
+    ],
+    imgUrl: 'xxxxxxx.jpg',
+    location: 'xxxx.xxx,xx.xxxx',
+  },
+}
---
+++ client/data/stations.temp.js
@@ -0,0 +1,45 @@
+export default {
+  xxxx: [
+    {
+      toilet_inside: false,
+      stat_id: 'xxxxx',
+      name_cn: 'xxx',
+      longitude: 'xxxx',
+      fltime: [
+        {
+          first_time_yc: '',
+          last_time_yc: 'xx:xx',
+          first_time: '',
+          description: 'xxxx',
+          last_time: 'xx:xx',
+        },
+        {
+          first_time_yc: 'xx:xx',
+          last_time_yc: 'xx:xx',
+          first_time: 'xx:xx',
+          description: 'xxxx',
+          last_time: 'xx:xx',
+        },
+        {
+          first_time_yc: 'xx:xx',
+          last_time_yc: 'xx:xx',
+          first_time: 'xx:xx',
+          description: 'xxxx',
+          last_time: 'xx:xx',
+        },
+        {
+          first_time_yc: 'xx:xx',
+          last_time_yc: 'xx:xx',
+          first_time: 'xx:xx',
+          description: 'xxx',
+          last_time: 'xx:xx',
+        },
+      ],
+      toilet_cn: ['', ''],
+      toilet_en: ['', ''],
+      latitude: 'xxxxxx',
+      line: 'xx',
+      name_en: 'xxx',
+    },
+  ],
+}
